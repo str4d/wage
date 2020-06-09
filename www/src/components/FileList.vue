814c15c9024ce9ab8de23a919966e034f6b28a39
Jack Grigg <thestr4d@gmail.com>
2020-06-09 15:59:54 +1200

    Add type icons to list of files to encrypt

--- www/src/components/FileList.vue
+++ www/src/components/FileList.vue
@@ -1,6 +1,9 @@
 <template>
-  <ul id="file-list">
+  <ul id="file-list" class="fa-ul">
     <li v-for="(file, index) in files" :key="file.name">
+      <span class="fa-li">
+        <font-awesome-icon :icon="getClassNameForMimeType(file.type).substring(3)" />
+      </span>
       {{ file.name }} ({{ file.size }} b)
       <button @click="removeFile(index)" title="Remove">X</button>
     </li>
@@ -8,10 +11,13 @@
 </template>
 
 <script>
+import { getClassNameForMimeType } from "font-awesome-filetypes";
+
 export default {
   name: "FileList",
   props: ["files"],
   methods: {
+    getClassNameForMimeType,
     removeFile(index) {
       this.$emit("file-removed", index);
     }
--- www/src/main.js
+++ www/src/main.js
@@ -12,9 +12,32 @@ streamSaver.mitm.search = `version=${streamSaver.version.full}`
 window.streamSaver = streamSaver;
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faFile, faFileArchive } from "@fortawesome/free-solid-svg-icons";
+import {
+  faFile,
+  faFileAlt,
+  faFileArchive,
+  faFileAudio,
+  faFileCode,
+  faFileCsv,
+  faFileExcel,
+  faFileImage,
+  faFilePdf,
+  faFilePowerpoint,
+  faFileVideo,
+  faFileWord,
+} from "@fortawesome/free-solid-svg-icons";
 library.add(faFile);
+library.add(faFileAlt);
 library.add(faFileArchive);
+library.add(faFileAudio);
+library.add(faFileCode);
+library.add(faFileCsv);
+library.add(faFileExcel);
+library.add(faFileImage);
+library.add(faFilePdf);
+library.add(faFilePowerpoint);
+library.add(faFileVideo);
+library.add(faFileWord);
 
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 Vue.component('font-awesome-icon', FontAwesomeIcon);
