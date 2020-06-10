b7366dc88782fb97c5d2dec4fc6c3d8d501608d5
Jack Grigg <thestr4d@gmail.com>
2020-06-10 13:04:20 +1200

    Add a passphrase input and button to EncryptPane

--- www/src/components/EncryptPane.vue
+++ www/src/components/EncryptPane.vue
@@ -2,11 +2,35 @@
   <div>
     <font-awesome-icon icon="file" size="10x" />
     <p>TODO: encryption pane.</p>
+    <div id="passphrase-section">
+      <label for="passphrase">Passphrase:</label>
+      <input type="password" id="passphrase" v-model="passphrase" />
+    </div>
+    <label class="button" @click="encryptFile">Encrypt with passphrase</label>
   </div>
 </template>
 
 <script>
 export default {
   name: "EncryptPane",
+  data() {
+    return {
+      passphrase: null,
+    };
+  },
+  methods: {
+    encryptFile() {
+      this.$emit("encrypt-with-passphrase", this.passphrase);
+    },
+  },
 };
 </script>
+
+<style scoped>
+#passphrase-section {
+  margin-bottom: 10px;
+}
+#passphrase {
+  margin-left: 10px;
+}
+</style>
